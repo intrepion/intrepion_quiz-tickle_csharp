600671ea8caff7b78d1d7b1bcda7670e88b1535a
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:20:38 -0600

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Answers\") }; { print }" Intrepion.QuizTickle.BusinessLogic/Entities/Configuration/AnswerEntityTypeConfiguration.cs

--- Intrepion.QuizTickle.BusinessLogic/Entities/Configuration/AnswerEntityTypeConfiguration.cs
+++ Intrepion.QuizTickle.BusinessLogic/Entities/Configuration/AnswerEntityTypeConfiguration.cs
@@ -7,13 +7,13 @@ public class AnswerEntityTypeConfiguration : IEntityTypeConfiguration<Answer>
 {
     public void Configure(EntityTypeBuilder<Answer> builder)
     {
-        builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
+        builder.ToTable("Answers", x => x.IsTemporal());
 
         // EntityConfigurationCodePlaceholder
         // builder.Property(x => x.PropertyNamePlaceholder);
 
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
-            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .WithMany(x => x.UpdatedAnswers)
             .OnDelete(DeleteBehavior.Restrict);
     }
 }
