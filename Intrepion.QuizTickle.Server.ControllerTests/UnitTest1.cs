83c004c3dcaef19ff90e520246a0b04ad5f8a1f9
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 20:54:38 -0600

    dotnet new nunit --name Intrepion.QuizTickle.Server.ControllerTests

---
+++ Intrepion.QuizTickle.Server.ControllerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Intrepion.QuizTickle.Server.ControllerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
