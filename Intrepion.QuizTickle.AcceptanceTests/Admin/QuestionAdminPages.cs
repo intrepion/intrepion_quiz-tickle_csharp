f7720ee282a21ca2ff0e40b1b00e1145d7d55b99
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:21:49 -0600

    gawk -i inplace "{ gsub(/PluralNamePlaceholder/, \"Questions\") }; { print }" Intrepion.QuizTickle.AcceptanceTests/Admin/QuestionAdminPages.cs

--- Intrepion.QuizTickle.AcceptanceTests/Admin/QuestionAdminPages.cs
+++ Intrepion.QuizTickle.AcceptanceTests/Admin/QuestionAdminPages.cs
@@ -11,7 +11,7 @@ public partial class QuestionAdminPages : PageTest
     public async Task MainNavigation()
     {
         await Expect(Page).ToHaveTitleAsync("Home");
-        await Page.GetByRole(AriaRole.Link, new() { Name = "PluralNamePlaceholder" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Questions" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Question List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Question Creation");
