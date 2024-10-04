17ba7f6e6003f9a7e96200943f15a70fd8dbd78b
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:22:55 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"QuestionType\") }; { print }" Intrepion.QuizTickle.AcceptanceTests/Admin/QuestionTypeAdminPages.cs

--- Intrepion.QuizTickle.AcceptanceTests/Admin/QuestionTypeAdminPages.cs
+++ Intrepion.QuizTickle.AcceptanceTests/Admin/QuestionTypeAdminPages.cs
@@ -5,7 +5,7 @@ namespace ApplicationNamePlaceholder.AcceptanceTests.Admin;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public partial class EntityNamePlaceholderAdminPages : PageTest
+public partial class QuestionTypeAdminPages : PageTest
 {
     [Test]
     public async Task MainNavigation()
