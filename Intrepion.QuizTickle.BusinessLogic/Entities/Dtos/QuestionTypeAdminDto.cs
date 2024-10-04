b140056cc12ce355fcd82d81bca8b05f39ace22a
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:23:04 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"questionType\") }; { print }" Intrepion.QuizTickle.BusinessLogic/Entities/Dtos/QuestionTypeAdminDto.cs

--- Intrepion.QuizTickle.BusinessLogic/Entities/Dtos/QuestionTypeAdminDto.cs
+++ Intrepion.QuizTickle.BusinessLogic/Entities/Dtos/QuestionTypeAdminDto.cs
@@ -9,33 +9,33 @@ public class EntityNamePlaceholderAdminDto
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
 
-    public static EntityNamePlaceholderAdminDto FromEntityNamePlaceholder(EntityNamePlaceholder? EntityLowercaseNamePlaceholder)
+    public static EntityNamePlaceholderAdminDto FromEntityNamePlaceholder(EntityNamePlaceholder? questionType)
     {
-        if (EntityLowercaseNamePlaceholder == null)
+        if (questionType == null)
         {
             return new EntityNamePlaceholderAdminDto();
         }
 
         return new EntityNamePlaceholderAdminDto
         {
-            Id = EntityLowercaseNamePlaceholder.Id,
+            Id = questionType.Id,
 
             // EntityToDtoPlaceholder
-            // Title = EntityLowercaseNamePlaceholder.Title,
-            // ToDoList = EntityLowercaseNamePlaceholder.ToDoList,
+            // Title = questionType.Title,
+            // ToDoList = questionType.ToDoList,
         };
     }
 
-    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser applicationUser, EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser applicationUser, EntityNamePlaceholderAdminDto questionTypeAdminDto)
     {
         return new EntityNamePlaceholder
         {
             ApplicationUserUpdatedBy = applicationUser,
-            Id = EntityLowercaseNamePlaceholderAdminDto.Id,
+            Id = questionTypeAdminDto.Id,
 
             // DtoToEntityPropertyPlaceholder
-            // Title = EntityLowercaseNamePlaceholderAdminDto.Title,
-            // ToDoList = EntityLowercaseNamePlaceholderAdminDto.ToDoList,
+            // Title = questionTypeAdminDto.Title,
+            // ToDoList = questionTypeAdminDto.ToDoList,
         };
     }
 }
