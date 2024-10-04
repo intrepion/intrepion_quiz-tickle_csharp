5a8d44972170d61e0037d4e68dde3b3e8353d753
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:24:24 -0600

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByTestId(\\\"quizAdminEditName\\\").FillAsync(\\\"a quiz\\\");\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" Intrepion.QuizTickle.AcceptanceTests/Admin/QuizAdminPages.cs

--- Intrepion.QuizTickle.AcceptanceTests/Admin/QuizAdminPages.cs
+++ Intrepion.QuizTickle.AcceptanceTests/Admin/QuizAdminPages.cs
@@ -16,6 +16,7 @@ public partial class QuizAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Quiz Creation");
 
+        await Page.GetByTestId("quizAdminEditName").FillAsync("a quiz");
         // CreatePropertyCodePlaceholder
         // await Page.GetByTestId("quizAdminEditName").FillAsync("a quiz");
 
