a522e06e78a416d9f18a20549b744e31cff3c89c
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:23:54 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"quiz\") }; { print }" Intrepion.QuizTickle.AcceptanceTests/Admin/QuizAdminPages.cs

--- Intrepion.QuizTickle.AcceptanceTests/Admin/QuizAdminPages.cs
+++ Intrepion.QuizTickle.AcceptanceTests/Admin/QuizAdminPages.cs
@@ -17,13 +17,13 @@ public partial class EntityNamePlaceholderAdminPages : PageTest
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Creation");
 
         // CreatePropertyCodePlaceholder
-        // await Page.GetByTestId("EntityLowercaseNamePlaceholderAdminEditName").FillAsync("a EntityLowercaseNamePlaceholder");
+        // await Page.GetByTestId("quizAdminEditName").FillAsync("a quiz");
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Modification");
 
         // ModifyPropertyCodePlaceholder
-        // await Page.GetByTestId("EntityLowercaseNamePlaceholderAdminEditName").FillAsync("some EntityLowercaseNamePlaceholder");
+        // await Page.GetByTestId("quizAdminEditName").FillAsync("some quiz");
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Modify" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Modification");
