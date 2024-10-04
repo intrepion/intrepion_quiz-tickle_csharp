43e394d51acf5e100d71df5c5a187674bce43b91
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:24:25 -0600

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string Name { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.QuizTickle.BusinessLogic/Entities/Quiz.cs

--- Intrepion.QuizTickle.BusinessLogic/Entities/Quiz.cs
+++ Intrepion.QuizTickle.BusinessLogic/Entities/Quiz.cs
@@ -5,5 +5,6 @@ public class Quiz
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public string Name { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
