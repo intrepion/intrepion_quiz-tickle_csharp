e858ff8a02cd0a4b8d8402dd58891861a03b5149
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-03 21:21:58 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Question\") }; { print }" Intrepion.QuizTickle.BusinessLogic/Services/Client/QuestionClientAdminService.cs

--- Intrepion.QuizTickle.BusinessLogic/Services/Client/QuestionClientAdminService.cs
+++ Intrepion.QuizTickle.BusinessLogic/Services/Client/QuestionClientAdminService.cs
@@ -4,15 +4,15 @@ using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Services.Client;
 
-public class EntityNamePlaceholderClientAdminService(HttpClient httpClient) : IEntityNamePlaceholderAdminService
+public class QuestionClientAdminService(HttpClient httpClient) : IQuestionAdminService
 {
     private readonly HttpClient _httpClient = httpClient;
 
-    public async Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto questionAdminDto)
+    public async Task<QuestionAdminDto?> AddAsync(QuestionAdminDto questionAdminDto)
     {
         var result = await _httpClient.PostAsJsonAsync("/api/admin/questionAdmin", questionAdminDto);
 
-        return await result.Content.ReadFromJsonAsync<EntityNamePlaceholderAdminDto>();
+        return await result.Content.ReadFromJsonAsync<QuestionAdminDto>();
     }
 
     public async Task<bool> DeleteAsync(string userName, Guid id)
@@ -22,23 +22,23 @@ public class EntityNamePlaceholderClientAdminService(HttpClient httpClient) : IE
         return await result.Content.ReadFromJsonAsync<bool>();
     }
 
-    public async Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto questionAdminDto)
+    public async Task<QuestionAdminDto?> EditAsync(QuestionAdminDto questionAdminDto)
     {
         var result = await _httpClient.PutAsJsonAsync($"/api/admin/questionAdmin/{questionAdminDto.Id}", questionAdminDto);
 
-        return await result.Content.ReadFromJsonAsync<EntityNamePlaceholderAdminDto>();
+        return await result.Content.ReadFromJsonAsync<QuestionAdminDto>();
     }
 
-    public async Task<List<EntityNamePlaceholder>?> GetAllAsync(string userName)
+    public async Task<List<Question>?> GetAllAsync(string userName)
     {
-        var result = await _httpClient.GetFromJsonAsync<List<EntityNamePlaceholder>>($"/api/admin/questionAdmin?userName={userName}");
+        var result = await _httpClient.GetFromJsonAsync<List<Question>>($"/api/admin/questionAdmin?userName={userName}");
 
         return result;
     }
 
-    public async Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id)
+    public async Task<QuestionAdminDto?> GetByIdAsync(string userName, Guid id)
     {
-        var result = await _httpClient.GetFromJsonAsync<EntityNamePlaceholderAdminDto>($"/api/admin/questionAdmin/{id}?userName={userName}");
+        var result = await _httpClient.GetFromJsonAsync<QuestionAdminDto>($"/api/admin/questionAdmin/{id}?userName={userName}");
 
         return result;
     }
